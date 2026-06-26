eac9ec0e20d2a9e4596a4363983b252c881d9934
fitrinoviantih <fitrinoviantihidayah@gmail.com>
2026-06-26 14:12:03 +0700

    data_scraping.py

--- data_scraping.py
+++ data_scraping.py
@@ -1,4 +1,8 @@
-# 1. Download for scraper's library from Google Play
+# 1. Download for scraper's library from Google Play and Pandas
 # !pip install google-play-scraper
+# !pip install pandas
 
-#
+# 2. Import the library
+from google_play_scraper import app
+import pandas as pd 
+
